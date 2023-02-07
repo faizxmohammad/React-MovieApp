ed836aff2b9e997599adda342c3ab94a789e547d
Faiz Mohammad <81904684+faizxmohammad@users.noreply.github.com>
2023-02-07 21:36:28 +0530

    Updates

---
+++ precache-manifest.61327fcc5812e4822058451af43e5590.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eae47e85a9b07946566",
+    "url": "/React-MovieApp/static/css/main.878ba0de.chunk.css"
+  },
+  {
+    "revision": "9eae47e85a9b07946566",
+    "url": "/React-MovieApp/static/js/main.9eae47e8.chunk.js"
+  },
+  {
+    "revision": "0f42ad21f20e3536abc0",
+    "url": "/React-MovieApp/static/js/1.0f42ad21.chunk.js"
+  },
+  {
+    "revision": "d954547ff7b5a4016df3",
+    "url": "/React-MovieApp/static/js/2.d954547f.chunk.js"
+  },
+  {
+    "revision": "94d4400e7200d636059c",
+    "url": "/React-MovieApp/static/js/runtime~main.94d4400e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/React-MovieApp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "26c741b266abaeae38183cb04221646b",
+    "url": "/React-MovieApp/index.html"
+  }
+];
